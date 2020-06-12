adf7c53c634da76752d005fd58b287369f02aa2d
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2020-06-11 23:43:03 -0300

    Decrease deployment target

--- Aquino/ServiceError.swift
+++ Aquino/ServiceError.swift
@@ -12,4 +12,6 @@ public enum ServiceError: String, Error {
     case parametersNil = "Parameters were nil."
     case encodingFailed = "Parameters encoding failed."
     case missingURL = "URL is nil."
+    case notPossibleRetrieve = "Could not retrieve data."
+    case parseError = "Parse error."
 }
